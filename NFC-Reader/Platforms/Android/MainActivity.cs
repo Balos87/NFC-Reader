b23dd3c9bd889b61fced78f3b20c2773d5466bd3
mmxzps <mojtaba.mobasheri@hotmail.com>
2024-12-04 21:58:11 +0100

    Added onCreate method with initializing CrossNFC

--- NFC-Reader/Platforms/Android/MainActivity.cs
+++ NFC-Reader/Platforms/Android/MainActivity.cs
@@ -1,11 +1,19 @@
 ﻿using Android.App;
 using Android.Content.PM;
 using Android.OS;
+using Plugin.NFC;
 
 namespace NFC_Reader
 {
     [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, LaunchMode = LaunchMode.SingleTop, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
     public class MainActivity : MauiAppCompatActivity
     {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            // initializing NFC-plugin to make sure its runs before base.OnCreate
+            CrossNFC.Init(this);
+
+            base.OnCreate(savedInstanceState);
+        }
     }
 }
